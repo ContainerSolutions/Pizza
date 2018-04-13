fe315ab8a4d5a2d74c71363fd9f764fb1dec6ea8
Adrian Mouat <adrian.mouat@gmail.com>
2018-04-13 16:49:52 -0400

    Various stuff...

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,23 @@
-FROM rust:latest
+FROM rust:latest as builder
 
 RUN rustup update nightly && rustup default nightly;
 WORKDIR /usr/src/pizza
-COPY . .
-RUN cargo build
 
+COPY Cargo.toml .
+COPY Cargo.lock .
+RUN mkdir src/
+RUN echo "fn main() {}" > src/main.rs
+RUN cargo build --release
+RUN rm /usr/src/pizza/target/release/pizza
+COPY src src
+RUN cargo build --release
+
+
+FROM debian:stable-slim
+
+COPY --from=builder /usr/src/pizza/target/release/pizza /pizza
+COPY img /img
+CMD /pizza
+ARG PIZZA="Quattro Formaggi"
+ENV PIZZA $PIZZA
 EXPOSE 8000
-CMD /usr/src/pizza/target/debug/pizza
-
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,10 @@ use failure::Error;
 
 use std::path::{Path, PathBuf};
 
-use rocket::response::{self, Responder, content, NamedFile};
+use rocket::config::{Config, Environment};
 use rocket::request::Request;
+use rocket::response::{self, content, NamedFile, Responder};
+use std::env;
 
 pub struct Pizza {
     name: String,
@@ -29,7 +31,7 @@ impl Pizza {
             <title>{}</title>
         </head>
         <body>
-            <h1>Today's pizza is {}</h1>
+            <h1>Today's Pizza is {}</h1>
             <img src="{}" width="400"/>
             <p>{}</p>
         </body>
@@ -40,7 +42,6 @@ impl Pizza {
 }
 
 impl<'a> Responder<'a> for Pizza {
- 
     fn respond_to(self, req: &Request) -> response::Result<'a> {
         content::Html(self.html()).respond_to(req)
     }
@@ -80,9 +81,22 @@ fn images(file: PathBuf) -> Option<NamedFile> {
 
 #[get("/")]
 pub fn index() -> Result<Pizza, Error> {
-    kitchen("Hawaiian")
+    match env::var("PIZZA") {
+        Ok(flavour) => {
+            println!("flavour");
+            kitchen(&flavour)
+        }
+        _ => kitchen("Hawaiian"),
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, images]).launch();
+    let config = Config::build(Environment::Production)
+        .address("0.0.0.0")
+        .port(8000)
+        .finalize()
+        .unwrap();
+
+    let app = rocket::custom(config, false);
+    app.mount("/", routes![index, images]).launch();
 }
